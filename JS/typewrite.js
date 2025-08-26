2b03c0a632a02bfb4351832308cd36147efafab4
Arch_user_202 <grimmikinflame@gmail.com>
2025-08-26 20:23:16 +0530

    Skeleton

---
+++ JS/typewrite.js
@@ -0,0 +1,65 @@
+class TypeWriter {
+  constructor(element, texts, options = {}) {
+    this.element = element;
+    this.texts = texts;
+    this.typingSpeed = options.typingSpeed || 100;
+    this.deletingSpeed = options.deletingSpeed || 50;
+    this.pauseDuration = options.pauseDuration || 1500;
+    this.loop = options.loop !== false; // default true
+    this.cursor = options.cursor || "|";
+    this.cursorBlink = options.cursorBlink !== false;
+    this.text = "";
+    this.textIndex = 0;
+    this.charIndex = 0;
+    this.isDeleting = false;
+
+    if (this.cursorBlink) {
+      this._createCursor();
+    }
+
+    this._type();
+  }
+
+  _createCursor() {
+    const cursorEl = document.createElement("span");
+    cursorEl.className = "typewriter-cursor";
+    cursorEl.textContent = this.cursor;
+    this.element.insertAdjacentElement("afterend", cursorEl);
+
+    setInterval(() => {
+      cursorEl.style.visibility =
+        cursorEl.style.visibility === "hidden" ? "visible" : "hidden";
+    }, 500);
+  }
+
+  _type() {
+    const currentText = this.texts[this.textIndex];
+
+    if (this.isDeleting) {
+      this.text = currentText.substring(0, this.text.length - 1);
+    } else {
+      this.text = currentText.substring(0, this.text.length + 1);
+    }
+
+    this.element.textContent = this.text;
+
+    let delay = this.isDeleting ? this.deletingSpeed : this.typingSpeed;
+
+    if (!this.isDeleting && this.text === currentText) {
+      delay = this.pauseDuration;
+      this.isDeleting = true;
+    } else if (this.isDeleting && this.text === "") {
+      this.isDeleting = false;
+      this.textIndex++;
+      if (this.textIndex >= this.texts.length) {
+        if (this.loop) {
+          this.textIndex = 0;
+        } else {
+          return; // stop if not looping
+        }
+      }
+    }
+
+    setTimeout(() => this._type(), delay);
+  }
+}
